ee6473bd859e232b49aa7fd3f7fddbf3c9a36b18
pavitsu <33056397+pavitsu@users.noreply.github.com>
2018-04-10 11:33:24 +0700

    Light sensor with scaling potentialmeter
    -add potentialmeter at A1
    -scaling the light by value it
    -add light sensor
    -for turn on/off light

---
+++ LDR.ino
@@ -0,0 +1,29 @@
+#include <LiquidCrystal.h>
+unsigned int rd;
+int sensorPin = A0; // select the input pin for LDR
+
+ 
+int sensorValue = 0; // variable to store the value coming from the sensor 
+void setup() { 
+  Serial.begin(9600); //sets serial port for communication
+
+} 
+void loop() {
+
+rd = analogRead(A1);
+analogWrite(9,rd/4);
+
+sensorValue = analogRead(sensorPin); // read the value from the sensor 
+  if(sensorValue<155){
+    Serial.println("SENSOR <100 ALERT!!!!");
+    analogWrite(9,rd/4);
+  }else{
+    digitalWrite(9,0);
+  }
+  Serial.println(sensorValue); //prints the values coming from the sensor on the screen 
+  delay(125); 
+}
+
+
+
+
